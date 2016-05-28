4ccd8fa667471a699a2c61470a97586999615ee6
Conner Lacy <connerlacy@connerlacy-mini.local>
2016-05-28 16:53:44 -0700

    - some basic boiler plate I don’t want to lose - MidiMessage - MidiMessageBuffer - …

--- arduino_midi.h
+++ arduino_midi.h
@@ -12,22 +12,58 @@
 #include <stdio.h>
 #include "Arduino.h"
 
+class MidiMessage;
+class MidiMessageBuffer;
+
 class Midi
 {
 public:
     
     Midi();
     
+    // ============= Init
     void start();
 
+    
+    // ============= Output
     void sendController(int channel, int cc, int value);
     void sendNoteOn(int channel, int note, int velocity);
     void sendNoteOff(int channel, int note, int velocity);
+    
+    
+    // ============= Input
+    MidiMessageBuffer getInputMidiMessages();
+    void appendInputMidiMessagesToBuffer(MidiMessageBuffer& buffer);
 
-
+    
 private:
 	int clampChannel(int channel);
 	int clampValue(int value);
+};
+
+class MidiMessage
+{
+public:
+    bool    isController();
+    bool    isNoteOn();
+    bool    inNoteOff();
+    bool    isNoteOnOrOff();
+    bool    isSysEx();
+    
+    int     getNumBytes();
+    
+    
+};
+
+class MidiMessageBuffer
+{
+public:
+    void clear();
+    void appendMidiMessage(MidiMessage message);
+    void prependMidiMessage(MidiMessage message);
+    int  getNumMessages();
+    MidiMessage getMessageAtIndex(int index)
+    
 };
 
 #endif /* arduino_midi_h */
